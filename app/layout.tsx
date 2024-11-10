fd14b1b1c7c66f516457e12ce5c0e58d37739e3b
Sarikhin <sarikhinlovestory@gmail.com>
2024-11-10 12:56:54 +0700

    [update]change meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Sarikhin",
-  description: "Fullstack Developer",
+  title: "Sarikhin - Fullstack Developer",
+  description: "Portfolio pribadi Sarikhin, seorang Fullstack Developer berpengalaman yang menguasai Nuxt, React, Go (Golang), dan Flutter untuk membangun aplikasi web dan mobile yang modern dan efisien. ",
 };
 
 export default function RootLayout({
@@ -25,6 +25,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="author" content="Sarikhin" />
+      <meta name="keywords" content="Fullstack Developer, Nuxt, React, Go, Golang, Flutter, Web Development, Mobile Development, JavaScript, TypeScript, Backend, Frontend, Portfolio" />
+
+      <meta property="og:title" content="Portfolio | Sarikhin - Fullstack Developer" />
+      <meta property="og:description" content="Lihat portofolio Sarikhin, Fullstack Developer yang berpengalaman dalam pengembangan aplikasi menggunakan Nuxt, React, Go (Golang), dan Flutter." />
+      <meta property="og:image" content="/profile.png " />
+      <meta property="og:url" content="/profile.png" />
+      <meta property="og:type" content="website" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Portfolio | Sarikhin - Fullstack Developer" />
+      <meta name="twitter:description" content="Lihat portofolio Sarikhin, Fullstack Developer yang menguasai Nuxt, React, Go (Golang), dan Flutter." />
+      <meta name="twitter:image" content="/profile.png" />
+      <meta name="twitter:url" content="/profile.png " />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
