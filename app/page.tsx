32fe68a45329abfbf587a62c4eca4e08e387eb1b
Sarikhin <sarikhinlovestory@gmail.com>
2024-11-10 12:46:07 +0700

    [update]change logo and add profile

--- app/page.tsx
+++ app/page.tsx
@@ -3,62 +3,71 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div style={{ textAlign: 'center', marginTop: '200px', maxWidth: "500px", minWidth: "500px", height: "auto" }} >
-        <pre style={{ whiteSpace: 'pre-wrap', lineHeight: 1, fontFamily: "monospace", overflow: "hidden", color: "#89B4FA", height: "auto" }} >
-          ███████╗ █████╗ ██████╗ ██╗██╗  ██╗██╗  ██╗██╗███╗   ██╗
-          ██╔════╝██╔══██╗██╔══██╗██║██║ ██╔╝██║  ██║██║████╗  ██║
-          ███████╗███████║██████╔╝██║█████╔╝ ███████║██║██╔██╗ ██║
-          ╚════██║██╔══██║██╔══██╗██║██╔═██╗ ██╔══██║██║██║╚██╗██║
-          ███████║██║  ██║██║  ██║██║██║  ██╗██║  ██║██║██║ ╚████║
-          ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝
-        </pre>
-        <div className="row-start-3 flex gap-6 flex-wrap flex-col items-center justify-center" >
-
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
+      <div style={{ textAlign: 'center', marginTop: '200px', }} >
+        <Image
+          aria-hidden
+          src="/logo.svg"
+          alt="Logo"
+          width={500}
+          height={120}
+        />
+        <h2 className="text-2xl font-extrabold">Fullstack Developer</h2>
+        <p></p>
+        <div className="flex gap-2 flex-wrap flex-col" >
+          <div className="flex justify-between">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-[#94E2D5] font-bold"
+              target="_blank"
+              href="/"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="/file.svg"
+                alt="Home"
+                width={16}
+                height={16}
+              />
+              Home
+            </a>
+            <p className="text-[#F9E2AF] font-bold" >h</p>
+          </div>
+          <div className="flex justify-between">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-[#94E2D5] font-bold"
+              target="_blank"
+              href="/projects"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="/window.svg"
+                alt="Projects"
+                width={16}
+                height={16}
+              />
+              Projects
+            </a>
+            <p className="text-[#F9E2AF] font-bold"> p</p>
+          </div>
+          <div className="flex justify-between">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-[#94E2D5] font-bold"
+              href="/about"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="/globe.svg"
+                alt="About Me"
+                width={16}
+                height={16}
+              />
+              About Me
+            </a>
+            <p className="text-[#F9E2AF] font-bold"> a</p>
+          </div>
         </div>
       </div >
     </div >
